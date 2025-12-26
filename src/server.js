78528a60123cb74865701e744fb5dd0b626936f3
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-12-26 12:44:29 +0530

    Done some fixes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.routes.js';
 import analysisRoutes from './routes/analysis.routes.js';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
